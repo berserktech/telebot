74190cdc08a9d3316ef68657ed29438ba391a07f
Daniel Rodríguez <sadasant@gmail.com>
2019-03-05 04:38:58 +0000

    zeit is back
    Zeit.co needs a main package with the Handler exposed, so I moved
    handler/handler.go to index.go :) now we know.

--- fmt-check.bash
+++ fmt-check.bash
@@ -1,3 +1,3 @@
+if [[ $(gofmt -l .) ]]; then exit 1; fi
 if [[ $(cd gh && gofmt -l .) ]]; then exit 1; fi
-if [[ $(cd handler && gofmt -l .) ]]; then exit 1; fi
 if [[ $(cd tg && gofmt -l .) ]]; then exit 1; fi
